414ef8df824e725f0a21453c88d1faef8888ac0c
Klay Clarke <klayaclarke@gmail.com>
2021-12-02 18:04:18 -0500

    accessing similar html elements using beautiful soup - use for loop to print individual / specific responses

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ yc_webpage_content = response.text
 
 soup = (BeautifulSoup(yc_webpage_content, 'html.parser'))
 
-article_tag = soup.find('a', class_='titlelink')
-article_text = article_tag.getText()
-article_link = article_tag.get('href')
-article_upvote = soup.find('span', class_='score').getText()
+
+
+# #accessing individual html items using BS
+# article_text = article_tag.getText()
+# article_link = article_tag.get('href')
+# article_upvote = soup.find('span', class_='score').getText()
+
+# accessing all similar items using BS
+articles = soup.find_all('a', class_='titlelink')
+for article_tag in articles:
+    article_text = article_tag.getText()
+    article_link = article_tag.get('href')
+    print(article_text)
+    print(article_link)
